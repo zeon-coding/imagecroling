6caaf9afde1e3fab4c20855a1c2c447423cf660e
zeon-coding <83157883+zeon-coding@users.noreply.github.com>
2021-09-04 11:53:33 +0900

    Index

--- selenium/chrome.py
+++ selenium/chrome.py
@@ -17,7 +17,7 @@ elem = driver.find_element_by_name("q")
 elem.send_keys(name)
 elem.send_keys(Keys.RETURN)
 
-SCROLL_PAUSE_TIME = 1.5
+SCROLL_PAUSE_TIME = 1
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
 while True:
@@ -39,7 +39,7 @@ count = 1
 for image in images:
     try:
         image.click()
-        time.sleep(2)
+        time.sleep(1.5)
         imgUrl = (driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img').get_attribute("src"))
         urllib.request.urlretrieve(imgUrl, name + str(count) + ".jpg")
         count = count + 1
